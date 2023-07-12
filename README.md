95b18d5129fa7800c436675d16b13472481005b3
zb3 <onlylogout@gmail.com>
2023-07-12 02:56:33 +0200

    It's 02:56 AM. What the hell am I doing?

---
+++ README.md
@@ -0,0 +1,123 @@
+
+# patch-tools
+
+Currently bindiff.
+
+## Bindiff
+
+The goal of this tool is to make it possible to distribute patches to (possibly multiple) binaries at a given path in a human-readable format that supports comments (and can be audited).
+
+### Rationale
+Say that there's an app called Lemodyne Studio which let's you pretend you're a bad singer (because you always hit every note perfectly and this doesn't sound so natural). It's not free, but it's easy to crack and you'd like to share this with as many singers as possible because you're super tired of constantly hearing only perfect vocal performances sounding like autotune (which you hate).
+
+So you want to distribute that crack, however, the app has a 80MB+ dll file that needs to be patched. Currently there are few problems with that:
+* if you wish to distribute the cracked binaries directly, you not only need to repack the installer but also need to find a host to host the whole file
+* that kind of distribution might violate copyright laws, and we of course don't want to violate our favorite law :(
+* the patch is not easy to audit, because it's not known what has been done to each file, additionally, the new installer seems completely opaque to the user, it's not known what it actually does
+* since all this is so cumbersome, it's rational to assume that that host 'd not just host it for free, but potentially bundle some malware inside
+* even if you wanted to make a classic binary crack program, the same security concerns still apply - it's not obvious what the program does and the binary needs to be hosted somewhere
+
+What if everything you need to share to make Lemodyne Studio available to everyone was just this piece of code:
+```diff
+>> Program Files/Celenomy/Lemodyne 5/Lemodyne.exe
+
+# disable bundle dll signature verification
+@0x00007076
+- 08 8d 7b f6
++ 00 48 31 ff
+
+>> Program Files/Common Files/Celenomy/Bundles/LemodyneCore-5.3.1.018.dll
+# disable wrapper exe signature verification
+@0x00e354ac
+- 07 02
++ 00 00
+@0x00e356d4
+- 32 c0
++ b0 01
+
+# not a crack - this just reenables 256 samples buffer size
+@0x0145bcd5
+- 02
++ 01
+
+# patch auth search - just return the first app
+@0x014cdcc0
+- e8 cb 09 00 00
++ b8 b5 00 7b fa
+
+# but set its type to 0x16 (the full studio edition)
+@0x014cdcdd
+- 18
++ 16
+
+# network patches not included, best to use something like unshare -Urn
+/*
+@0x01725bea
+- 8b d7
++ 31 d2
+*/
+```
+Wouldn't this be great? (I think I should win a nobel prize I mean I'm not bragging but ah just look at this...)
+
+So... the advantages of this approach are clear:
+* there's no need to create any custom installers or programs that apply the patch after installing
+* the patch file is easy to read, it's obvious that it doesn't contain any malware because there are simply not enough bytes to introduce one
+* the patch file contains comments, so it's even possible to let the user customize the patch (for example by removing undesired changes or adjusting some changed bytes)
+* the patch file is small and text-only, so it's easy to find a place to host it for free
+* the actual program can then be safely downloaded from the official website (no need to pay for traffic or violate (our favorite) copyright law :)
+
+Okay... so how to use bindiff? Let me explain.
+
+### Producing patches
+
+Let's say that the files to be patched are in these locations:
+```
+.../Program Files/Celenomy/Lemodyne 5/Lemodyne.exe
+.../Program Files/Common Files/Celenomy/Bundles/LemodyneCore-5.3.1.018.dll
+```
+This `...` part is system dependent, so it shouldn't be included in the patch. That's the base directory.
+
+Now suppose you saved the original files with the `.bak` suffix. Then the example command to produce the patch would be:
+```
+# change to the base directory (or specify it in the -d diff subcommand argument)
+cd ~/.wine/drive_c
+
+python bindiff.py diff \
+  'Program Files/Celenomy/Lemodyne 5/Lemodyne.exe.bak' 'Program Files/Celenomy/Lemodyne 5/Lemodyne.exe' -p \
+  'Program Files/Common Files/Celenomy/Bundles/LemodyneCore-5.3.1.018.dll.bak' 'Program Files/Common Files/Celenomy/Bundles/LemodyneCore-5.3.1.018.dll' -p
+```
+
+That's because the `diff` subcommand expects one or more files that are specified by a group of 3 arguments:
+```
+[original file] [patched file] [which name to use -o means the original file is now at the correct the path, -p means the patched file is now at the correct path, you can also supply custom path to be used inside the patch]
+```
+
+The command then outputs the patch on the standard output. You can manually annotate it by adding lines starting with `#` which are then treated as comments.
+
+### Applying them
+
+If you have the patch file and the original files installed at the correct location, applying the patch is super easy:
+```
+# change to the base directory (or specify it in the -d patch subcommand argument)
+cd ~/.wine/drive_c
+
+python bindiff.py patch </the/patch/file.patch
+```
+
+The original bytes are checked for being correct, so the program will never overwrite bytes that were already modified by the user. This prevents data loss, but also makes it possible to provide the undo functionality.
+Yes, you can undo the patch using the `-u` switch! (wow, what an impressing feature, isn't it?)
+
+```
+# change to the base directory
+cd ~/.wine/drive_c
+
+python bindiff.py patch -u </the/patch/file.patch
+```
+
+### bindiff FAQ:
+
+#### Q: Are there any elephants in Antarctica?
+**A:** Yes, but I'm the only one currently living there.
+
+#### Q: Can you play piano?
+**A:** I can play it like I can make it emit some noise, it's just that I can't tell which note will be played in advance.
---
+++ diff.py
@@ -0,0 +1,231 @@
+import argparse
+import binascii
+import os
+import mmap
+import sys
+
+from collections import defaultdict
+
+CHUNK_SIZE = 16
+MIN_NON_DIFF_BYTES = 16
+
+def _find_difference_end(bytes1, bytes2, i, total_len):
+    while True:
+        while i < total_len and bytes1[i] != bytes2[i]:
+            i += 1
+
+        if i == total_len:
+            return i
+
+        common_start = i
+        while i < total_len and i < common_start + MIN_NON_DIFF_BYTES and bytes1[i] == bytes2[i]:
+            i += 1
+
+        if i == total_len or i == common_start + MIN_NON_DIFF_BYTES:
+            return common_start
+
+
+def diff(file1, file2, basedir='.'):
+    with open(os.path.join(basedir, file1), "rb") as f1, open(os.path.join(basedir, file2), "rb") as f2:
+        with mmap.mmap(f1.fileno(), 0, access=mmap.ACCESS_READ) as bytes1, mmap.mmap(f2.fileno(), 0, access=mmap.ACCESS_READ) as bytes2:
+
+          total_len = min(len(bytes1), len(bytes2))
+
+          i = 0
+          while i < total_len:
+              if bytes1[i] == bytes2[i]:
+                  i += 1
+                  continue
+
+              start = i
+              i = _find_difference_end(bytes1, bytes2, i, total_len)
+
+              yield start, bytes1[start:i], bytes2[start:i]
+
+          if i < max(len(bytes1), len(bytes2)):
+              if len(bytes1) == total_len:
+                  yield i, None, bytes2[i:]
+              else:
+                  yield i, bytes1[i:], None
+
+
+def _chunks(n, size):
+    pos = 0
+    while pos < n:
+        yield pos, pos + size
+        pos += size
+
+
+def diff_command(diff_dir, file_groups):
+    for file1, file2, target_name in file_groups:
+        shown = False
+        for start, before, after in diff(file1, file2, basedir=diff_dir):
+            if not shown:
+                print(f">> {target_name}")
+                shown = True
+
+            print(f"@0x{start:08x}")
+
+            for data in [before, after]:
+                if data:
+                    for lstart, lend in _chunks(len(data), CHUNK_SIZE):
+                        prefix = "- " if data is before else "+ "
+                        print(prefix + " ".join(f"{byte:02x}" for byte in data[lstart:lend]))
+
+def parse_diff(unpatch=False):
+    patches = defaultdict(lambda: defaultdict(lambda: {'old': b'', 'new': b''}))  
+    current_file = None
+    current_offset = None
+
+    in_multiline_comment = False
+
+    for line in sys.stdin:
+        line = line.strip()
+        
+        if in_multiline_comment:
+            if '*/' in line:
+                in_multiline_comment = False
+            continue
+
+        if '/*' in line:
+            in_multiline_comment = True
+            continue
+                
+        if not line or line.startswith('#'):
+            continue
+
+        if line.startswith('>> '):
+            if current_offset is not None:
+                # append pending offset
+                patches[current_file][current_offset] = {"old": old_bytes, "new": new_bytes}
+            current_file = line[3:]
+            current_offset = None
+        elif line.startswith('@0x'):
+            if current_offset is not None:
+                # append pending offset
+                patches[current_file][current_offset] = {"old": old_bytes, "new": new_bytes}
+            current_offset = int(line[3:], 16)
+            old_bytes = new_bytes = b''
+        elif line.startswith('+ ' if unpatch else '- '):
+            old_bytes += bytes.fromhex(line[2:])
+        elif line.startswith('- ' if unpatch else '+ '):
+            new_bytes += bytes.fromhex(line[2:])
+
+    # append last offset
+    if current_file and current_offset is not None:
+        patches[current_file][current_offset] = {"old": old_bytes, "new": new_bytes}
+
+    return patches
+
+def verify_patches(patches, diff_dir='.'):
+    for filename, patches_for_file in patches.items():
+        with open(os.path.join(diff_dir, filename), 'r+b') as file:
+            for offset, patch in sorted(patches_for_file.items()):
+                file.seek(offset)
+
+                old_bytes = patch['old']
+                if file.read(len(old_bytes)) != old_bytes:
+                    raise ValueError(f'{filename}: bytes not equal at {offset:#0{4}x}.')
+
+
+def apply_patches(patches, diff_dir='.'):
+    for filename, patches_for_file in patches.items():
+        with open(os.path.join(diff_dir, filename), 'r+b') as file:
+            for offset, patch in sorted(patches_for_file.items()):
+                old_bytes, new_bytes = patch["old"], patch["new"]
+                if not new_bytes:
+                    continue
+
+                print(f'{filename}: writing {len(new_bytes)} bytes at {offset:#0{4}x}', file=sys.stderr)
+                file.seek(offset)
+                file.write(new_bytes)
+
+            if len(new_bytes) < len(old_bytes):  # bytes were removed and not fully replaced
+                num_to_truncate = len(old_bytes) - len(new_bytes)
+                print(f'{filename}: removing {num_to_truncate} bytes at the end', file=sys.stderr)
+                file.truncate(offset + len(new_bytes))  # truncate file
+
+
+def patch_command(diff_dir='.', unpatch=False):
+    patches = parse_diff(unpatch)
+
+    try:
+        verify_patches(patches, diff_dir)
+    except ValueError as ve:
+        print(f'Error: {ve.args[0]}', file=sys.stderr)
+        print('Aborting.')
+        exit(1)
+
+    apply_patches(patches, diff_dir)
+
+def custom_parser(args):
+    if not args or args[0] in ('-h', '--help') or len(args) % 3:
+        print(diff_help_text, file=sys.stderr)
+        exit(1)
+
+    parsed_args = []
+
+    for x in range(0, len(args), 3):
+        original, patched, path = args[x:x+3]
+
+        if path in ('-o', '--orig', '--original'):
+          path = original
+
+        elif path in ('-p', '--patch', '--patched'):
+          path = patched
+
+        parsed_args.append((original, patched, path))
+
+    return parsed_args
+
+
+diff_help_text = f'''
+usage: {sys.argv[0]} diff [-h] [-d PATCH_DIR] [file groups]...
+
+options:
+  -h, --help            show this help message and exit
+  -d PATCH_DIR, --patch-dir PATCH_DIR
+                        Directory to apply patches
+
+file group syntax:
+  each file group consists of 3 arguments - the original file, the patched file and the target path (the file name in the patch file).
+
+  for convenience, the target path might also be:
+  -o, --original        to use the path of the original file as the target file path
+  -p, --patched         to use the path of the pathed file as the target file path
+
+  examples:
+  python {sys.argv[0]} diff /tmp/file.original /opt/file -p
+  python {sys.argv[0]} diff /opt/file /tmp/file.patched -o
+  python {sys.argv[0]} diff /tmp/file.original /tmp/file.patched /opt/file
+
+  more than one group can be specified in order to generate diff for multiple files, for example:
+  python {sys.argv[0]} diff /tmp/file1.original /opt/file1 -p \\
+     /opt/file2 /tmp/file2.patched -o
+'''
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(dest='command', required=True)
+
+    diff_parser = subparsers.add_parser('diff', add_help=False)
+    diff_parser.add_argument('-d', '--patch-dir', default='.', help='Patch base directory')
+
+    patch_parser = subparsers.add_parser('patch')
+    patch_parser.add_argument('-d', '--patch-dir', default='.', help='Directory to apply patches')
+    patch_parser.add_argument('-u', '--unpatch', action='store_true', help='Experimental unpatch mode')
+
+    args, remainder = parser.parse_known_args()
+    # print(args, remainder)
+
+    if args.command == 'diff':
+        file_groups = custom_parser(remainder)
+        diff_command(args.patch_dir, file_groups)
+
+    elif args.command == 'patch':
+        patch_command(args.patch_dir, args.unpatch)
+
+if __name__ == '__main__':
+    main()
+
